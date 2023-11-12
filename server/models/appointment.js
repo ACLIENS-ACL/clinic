3680349c3d5dee62e4af722f37e9a8b2736d1730
mennan0809 <121335355+mennan0809@users.noreply.github.com>
2023-11-12 21:21:17 +0200

    Sprint#2

--- server/models/appointment.js
+++ server/models/appointment.js
@@ -43,11 +43,6 @@ const AppointmentSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
-  paid: {
-    type: Boolean,
-    default: false,
-  }
-
 });
 
 // Update the virtual field for 'status' to check 'cancelled' attribute
